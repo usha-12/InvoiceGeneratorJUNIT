f686b0ff108133a330e8e5e3a7df9f22a2a19703
usha-12 <usha12ahirwar@gmail.com>
2023-02-20 12:12:51 +0530

    Added: Calculate Fare

--- src/main/java/com/brideglabz/invoicegenerator/InvoiceGenerator.java
+++ src/main/java/com/brideglabz/invoicegenerator/InvoiceGenerator.java
@@ -1,2 +1,13 @@
-package com.brideglabz.invoicegenerator;public class InvoiceGenerator {
+package com.brideglabz.invoicegenerator;
+public class InvoiceGenerator {
+    private static final double Min_Cost_Per_KiloMeter = 10;
+    private static final double Cost_per_Time = 1;
+    private static final double Minimum_Fare = 5;
+
+    public double calculateFare(double distance, double time) {
+        double cost = distance * Min_Cost_Per_KiloMeter + time * Cost_per_Time;
+        if (cost < Minimum_Fare)
+            return Minimum_Fare;
+        return cost;
+    }
 }
--- src/test/java/com/brideglabz/invoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/brideglabz/invoicegenerator/InvoiceGeneratorTest.java
@@ -1,2 +1,32 @@
-package com.brideglabz.invoicegenerator;public class InvoiceGeneratorTest {
+package com.brideglabz.invoicegenerator;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceGeneratorTest {
+    InvoiceGenerator  invoiceGenerator = new InvoiceGenerator();
+    @BeforeAll
+    public static void printBeforeAllMsg() {
+        System.out.println("-------Welcome to the Cab Invoice Generator Program-------");
+    }
+    @AfterEach
+    public void PrintAfterEachTestCasePass(){
+        System.out.println("passed");
+    }
+    @Test
+    public void givenDistanceAndTime_shouldReturn_CalculateFare() {
+        double distance = 2.0;
+        double time = 5.0;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25.0, fare, 0.0);
+    }
+    @Test
+    public void givenLessDistanceAndTime_shouldReturn_MinimumCalculateFare() {
+        double distance = 0.1;
+        double time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
 }
